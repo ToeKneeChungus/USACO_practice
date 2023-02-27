0f864d52780406e2e8d1934428c170af824728cd
toekneechungus <chung.jiwook99@gmail.com>
2023-02-26 22:47:14 -0800

    hi

---
+++ Silver/prefix_sums/WDTCCTR2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ifstream fin("maxcross.in");
+    ofstream fout("maxcross.out");
+    int N,K,B;cin>>N>>K>>B;
+    vector<int>l;
+    for(int i=0;i<B;i++){
+        int t;cin>>t;
+        l.push_back(t);
+        sort(l.begin(),l.end());
+        
+    }
+}
